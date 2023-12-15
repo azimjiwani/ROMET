9be4c14a6f91c2c3fc08fc62d6adc64d6630bf06
Azim Jiwani <azimjiwani4@gmail.com>
2023-12-15 12:56:08 -0500

    Initial Commit

---
+++ Flexify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Flexify
+//
+//  Created by Azim Jiwani on 2023-12-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flexify/FlexifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlexifyApp.swift
+//  Flexify
+//
+//  Created by Azim Jiwani on 2023-12-15.
+//
+
+import SwiftUI
+
+@main
+struct FlexifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
