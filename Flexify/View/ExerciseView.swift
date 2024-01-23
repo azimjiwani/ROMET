b88509df8c52ea58531e216f8fe19ec28e4eb137
Azim Jiwani <azimjiwani4@gmail.com>
2024-01-22 19:39:37 -0500

    view clean up

--- Flexify/View/ExerciseView.swift
+++ Flexify/View/ExerciseView.swift
@@ -95,6 +95,7 @@ struct ExerciseView: View {
                 .frame(width: geometry.size.width)
                 .edgesIgnoringSafeArea(.all)
                 .onAppear {
+                    print("started")
                     quickPose.start(features: [], onFrame: { status, image, features, feedback, landmarks in
                         overlayImage = image
                         if case .success = status, let landmarks = landmarks {
@@ -138,7 +139,6 @@ struct ExerciseView: View {
                                 }
                                 
                                 previousAngle = angle
-                                print("Angle: \(angle), Reps: \(repCount)")
                             }
                             
                         } else {
@@ -147,6 +147,7 @@ struct ExerciseView: View {
                     })
                 }
                 .onDisappear {
+                    print("stopped")
                     quickPose.stop()
                 }
                 HStack {
@@ -167,107 +168,3 @@ struct ExerciseView: View {
         }
     }
 }
-
-
-
-//struct ExerciseView: View {
-//
-//    var viewModel: ExerciseViewModel
-//
-//
-//    @State var overlayImage: UIImage?
-//    @State var leftElbowPoint = CGPoint(x: 1080/2, y: 1920/2)
-//    @State var leftWristPoint = CGPoint(x: 1080/2, y: 1920/2)
-//    @State var leftMCP3Point = CGPoint(x: 1080/2, y: 1920/2)
-//    @State var angle: CGFloat = 0.0
-//
-//    var body: some View {
-//        VStack() {
-//            Text(viewModel.exercise?.name ?? "no name")
-//            Text(viewModel.exercise?.type?.rawValue.capitalized ?? "no type")
-//                .font(.subheadline)
-//                .foregroundColor(.secondary)
-//            Text(viewModel.exercise?.description ?? "no type")
-//                .font(.subheadline)
-//                .foregroundColor(.secondary)
-//        }
-//        GeometryReader { geometry in
-//            ZStack(alignment: .top) {
-//                QuickPoseCameraView(useFrontCamera: true, delegate: quickPose, videoGravity: .resizeAspect)
-//                QuickPoseOverlayView(overlayImage: $overlayImage, contentMode: .fit)
-//            }
-//            .overlay(alignment: .topLeading) {
-//                Circle()
-//                // elbow
-//                    .position(x: leftElbowPoint.x, y: leftElbowPoint.y)
-//                    .frame(width: 12, height: 12)
-//                    .foregroundColor(Color.green.opacity(1.0))
-//                // wrist
-//                Circle()
-//                    .position(x: leftWristPoint.x, y: leftWristPoint.y)
-//                    .frame(width: 12, height: 12)
-//                    .foregroundColor(Color.green.opacity(1.0))
-//                // MCP3
-//                Circle()
-//                    .position(x: leftMCP3Point.x, y: leftMCP3Point.y)
-//                    .frame(width: 12, height: 12)
-//                    .foregroundColor(Color.green.opacity(1.0))
-//
-//            }
-//            .overlay(alignment: .bottom) {
-//                Text("Angle: \(angle)") // remove logo here, but attribution appreciated
-//                    .font(.system(size: 16, weight: .semibold)).foregroundColor(.white)
-//                    .frame(maxHeight:  40 + geometry.safeAreaInsets.bottom, alignment: .center)
-//                    .padding(.bottom, 10)
-//            }
-//            .frame(width: geometry.size.width)
-//            .edgesIgnoringSafeArea(.all)
-//            .onAppear {
-//                quickPose.start(features: [], onFrame: { status, image, features, feedback, landmarks in
-//                    overlayImage = image
-//                    if case .success = status, let landmarks = landmarks {
-//
-//                        // left elbow
-//                        let leftElbow = landmarks.landmark(forBody: .elbow(side: .left))
-//                        leftElbowPoint = leftElbow.cgPoint(scaledTo: geometry.size, flippedHorizontally: true)
-//
-//                        // left wrist
-//                        let leftWrist = landmarks.landmark(forBody: .wrist(side: .left))
-//                        leftWristPoint = leftWrist.cgPoint(scaledTo: geometry.size, flippedHorizontally: true)
-//
-//                        // left mcp3
-//                        if let leftMCP3 = landmarks.landmark(forLeftHand: .MIDDLE_FINGER_TIP) {
-//                            leftMCP3Point = leftMCP3.cgPoint(scaledTo: geometry.size, flippedHorizontally: true)
-//                        }
-//
-//                        // compute joint angle
-//                        angle = 180 - (atan2(leftMCP3Point.y - leftWristPoint.y, leftMCP3Point.x - leftWristPoint.x) - atan2(leftElbowPoint.y - leftWristPoint.y, leftElbowPoint.x - leftWristPoint.x)) * 180 / .pi
-//
-//                        if angle < 0 {
-//                            angle += 360
-//                            if angle > 180 {
-//                                angle = 360 - angle
-//                            }
-//                        }
-//                        else if angle > 180 {
-//                            angle = 360 - angle
-//                        }
-//
-//                        print(angle)
-//
-//                        //                        if let nose = landmarks.landmark(forFace: .faceNose) {
-//                        //                            print(nose.cgPoint(scaledTo: geometry.size, flippedHorizontally: false))
-//                        //                            print(nose)
-//                        //                        }
-//                    } else {
-//                        // show error feedback
-//                    }
-//                })
-//            }.onDisappear {
-//                quickPose.stop()
-//            }
-//        }
-//        Spacer()
-//    }
-//}
-//
