0a3683761f73eb468e7f87b6a7eb49f8952a6c0e
Xu <coriolisxx616@gmail.com>
2024-03-13 11:57:27 -0400

    Added static instructions for wrist flexion and made TODO list to finish the rest

--- Flexify/View/InstructionsView.swift
+++ Flexify/View/InstructionsView.swift
@@ -14,12 +14,40 @@ struct InstructionsView: View {
     var body: some View {
         ZStack {
             Colours.backgroundColour.ignoresSafeArea()
-            VStack(alignment: .leading){
+            VStack(alignment: .center){
                 Text(viewModel.exercise?.name ?? "NULL")
                     .font(.largeTitle)
                     .fontWeight(.bold)
                     .padding(20)
                     .foregroundStyle(Colours.primaryTextColour)
+                
+                //TODO:
+                //Make instructions for all four exercises respectively
+                //Check the connection between the ExerciseListView and InstructionsView to ensure consistency
+                //Make wrapper for gifs
+                
+                // Insert images here
+                Image("Instructions/flex_neutral")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Text("Wrist positioned at neutral starting point")
+                .font(.caption)
+                .foregroundColor(.gray)
+                
+                Image("Instructions/flex_down")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Text("Wrist move down and hold at max angle for one second")
+                .font(.caption)
+                .foregroundColor(.gray)
+                
+                Image("Instructions/flex_up")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Text("Wrist move up and return to neutral position")
+                .font(.caption)
+                .foregroundColor(.gray)
+
                 Spacer()
                 NavigationLink(destination: ExerciseView(viewModel: viewModel, goBackToRoot: self.$goBackToRoot).toolbar(.hidden, for: .tabBar)) {
                     Text("Proceed To Exercise")
